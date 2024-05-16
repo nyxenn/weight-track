06051bc649fa6e44a155048ce755d633124951b1
Nyxenn <jordib.edu@gmail.com>
2024-05-16 19:55:04 +0200

    refactor: simplify db schema config
    db:push command would not push changes up to remote database if schema
    declaration was split over multiple files instead of all being defined
    in schema.ts.
    config files were modified accordingly to allow this behavior, deleted
    schema.ts in the process.

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,7 +2,8 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 import { env } from "~/env";
-import schema from "./schema";
+import * as workoutTables from "./workout";
+import * as exerciseTables from "./exercise";
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
@@ -14,5 +15,10 @@ const globalForDb = globalThis as unknown as {
 
 const conn = globalForDb.conn ?? postgres(env.POSTGRES_URL);
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+const schema = {
+  ...workoutTables,
+  ...exerciseTables,
+};
 
 export const db = drizzle(conn, { schema });
--- src/server/db/schema.ts
+++
@@ -1,9 +0,0 @@
-import * as workoutTables from "./workout";
-import * as exerciseTables from "./exercise";
-
-const schema = {
-  ...workoutTables,
-  ...exerciseTables,
-};
-
-export default schema;
