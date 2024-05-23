d83c10ece2162b49902a1a09c5fc826ffc0c0f3a
Nyxenn <jordib.edu@gmail.com>
2024-05-23 21:06:49 +0200

    Update card styling

--- src/app/_components/workout-card.tsx
+++ src/app/_components/workout-card.tsx
@@ -9,17 +9,34 @@ export default function WorkoutCard() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Temp 1</CardTitle>
-        <CardDescription>
-          <div className="flex flex-row items-end justify-between">
-            <div>Exercise 1, Exercise 2, ...</div>
-
-            <div>
-              <div>Previous</div>
-              <div>01/01/2024</div>
-            </div>
+        <div className="flex cursor-pointer flex-row justify-between">
+          <div>
+            <CardTitle>Temp 1</CardTitle>
+            <CardDescription>
+              <div>
+                <div>Exercise 1, Exercise 2, ...</div>
+                <div>Previous 01/01/2024</div>
+              </div>
+            </CardDescription>
           </div>
-        </CardDescription>
+
+          <div className="h-100 flex items-center p-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m8.25 4.5 7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </div>
+        </div>
       </CardHeader>
     </Card>
   );
